0322e87b9ab391f9525eb7cdf8e1f597159c2806
EdisonAltamirano <A00825234@itesm.mx>
2023-01-28 04:00:39 +0000

    Class inheritance - dynamic model

--- can_devices/sdv_control_ros2/src/model/car_simulation_node.cpp
+++ can_devices/sdv_control_ros2/src/model/car_simulation_node.cpp
@@ -17,9 +17,6 @@
 #include <pluginlib/class_loader.hpp>
 using namespace std::chrono_literals;
 
-/* This example creates a subclass of Node and uses std::bind() to register a
-* member function as a callback from the timer. */
-
 class CarSimulationNode : public rclcpp::Node
 {
   public:
