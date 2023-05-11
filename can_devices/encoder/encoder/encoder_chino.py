c34db7592c590190c8927825c368a10cf2b4352a
saucesaft <eduarch42@protonmail.com>
2023-05-11 01:39:33 -0600

    encoder chino specs & code

--- can_devices/encoder/encoder/encoder_chino.py
+++ can_devices/encoder/encoder/encoder_chino.py
@@ -5,6 +5,8 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int32, Int16, Float64
 
+# import usb
+
 class EncoderPublisher(Node):
 
     def __init__(self):
@@ -14,7 +16,16 @@ class EncoderPublisher(Node):
         self.publisher_angle = self.create_publisher(Float64, 'encoder/angle', 10)
         timer_period = 0.01 #Seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.bus = can.interface.Bus(bustype='socketcan', channel='can0', bitrate=500000)
+        self.bus = can.interface.Bus(bustype='socketcan', channel='can0', bitrate=125000)
+
+        # self.dev = usb.core.find(idVendor=0x1D50, idProduct=0x606F)
+        # self.bus = can.Bus(
+        #     interface="gs_usb",
+        #     channel=self.dev.product,
+        #     bus=self.dev.bus,
+        #     address=self.dev.address,
+        #     bitrate=125000,
+        # )
 
         self.sendMsg = can.Message(arbitration_id=1, is_extended_id=False, data=[0x04, 0x01, 0x01, 0x00])
 
@@ -41,22 +52,119 @@ class EncoderPublisher(Node):
                 # self.publisher_turn_number.publish(turNum)
                 self.publisher_angle.publish(angle)
 
+    # def read(self):
+    #     # self.bus.send(self.sendMsg, timeout=1)
+    #
+    #     receivedMsg = self.bus.recv(0)
+    #     if receivedMsg is not None:
+    #         if receivedMsg.is_rx:
+    #             # return int( (receivedMsg.data.hex()[6:])[::-1], 16)
+    #             return receivedMsg.data.hex()
+
+    # FUNCIONAL
     def cambiar_id(self, id, new):
         # command 0x02 - change device id
-        msg = can.Message(arbitration_id=1, is_extended_id=False, data=[0x04, id, 0x02, new])
+        msg = can.Message(arbitration_id=id, is_extended_id=False, data=[0x04, id, 0x02, new])
         self.bus.send( msg, timeout=1 )
 
+    # FUNCIONAL
     def query_mode(self, id):
         # command 0x04 - set encoder to query mode
-        msg = can.Message(arbitration_id=1, is_extended_id=False, data=[0x04, id, 0x04, 0x00])
+        msg = can.Message(arbitration_id=id, is_extended_id=False, data=[0x04, id, 0x04, 0x00])
+        self.bus.send( msg, timeout=1 )
+
+    # esto viene dentro del datasheet:
+    # Note: After setting a too short return time, the encoder will no longer be able to set other parameters, use it with caution
+    # microsegundos: 50 - 65535
+    # FUNCIONAL
+    def automatic_mode(self, id, microsegundos):
+        if microsegundos < 50 or microsegundos > 65535:
+            raise Exception('Introduce un valor entre 50 y 65535 microsegundos')
+
+        # command 0x04 - set encoder to automatic mode
+        msg = can.Message(arbitration_id=id, is_extended_id=False, data=[0x04, id, 0x04, 0xAA])
+        self.bus.send( msg, timeout=1 )
+
+        # command 0x05 - set automatic mode return interval
+        microsegundos = self.int2hex(microsegundos)
+        contenido_msg = [id, 0x05] + microsegundos
+        contenido_msg.insert(0, len(contenido_msg) + 1)
+
+        print(contenido_msg)
+
+        msg = can.Message(arbitration_id=id, is_extended_id=False, data=contenido_msg)
+        self.bus.send( msg, timeout=1 )
+
+    # NO FUNCIONAL
+    # # dirección en la manesillas del reloj
+    # def clockwise(self, id):
+    #     # command 0x07 - set the encoder's direction
+    #     msg = can.Message(arbitration_id=id, is_extended_id=False, data=[0x04, id, 0x07, 0x00])
+    #     self.bus.send( msg, timeout=1 )
+    #
+    # # dirección en contra de las manesillas del reloj
+    # def counter_clockwise(self, id):
+    #     # command 0x07 - set the encoder's direction
+    #     msg = can.Message(arbitration_id=id, is_extended_id=False, data=[0x04, id, 0x07, 0x01])
+    #     self.bus.send( msg, timeout=1 )
+
+    # valor de 0 a 1 (decimal)
+    # FUNCIONAL
+    def cambiar_posicion(self, id, pos):
+        abspos = int(pos * 65535)
+
+        # command 0x0D - set the encoder's position
+        abspos = self.int2hex(abspos)
+        contenido_msg = [id, 0x0D] + abspos
+        contenido_msg.insert(0, len(contenido_msg) + 1)
+
+        msg = can.Message(arbitration_id=id, is_extended_id=False, data=contenido_msg)
+        self.bus.send( msg, timeout=1 )
+
+    # resetea la posición a 0
+    # FUNCIONAL
+    def position_reset(self, id):
+        msg = can.Message(arbitration_id=id, is_extended_id=False, data=[0x04, id, 0x06, 0x00])
+        self.bus.send( msg, timeout=1 )
+
+    # 500K; 1M; 250K; 125K; 100K
+    # Cambia el baudrate de la interfaz tmb
+    # FUNCIONAL
+    def cambiar_baudrate(self, id, baud):
+
+        if baud == 500000:
+            baud = 0x00
+        elif baud == 1000000:
+            baud = 0x01
+        elif baud == 250000:
+            baud = 0x02
+        elif baud == 125000:
+            baud = 0x03
+        elif baud == 100000:
+            baud = 0x04
+        else:
+            raise Exception('Introduce un valor válido')
+
+        # command 0x03 - set the encoder's baudrate
+        msg = can.Message(arbitration_id=id, is_extended_id=False, data=[0x04, id, 0x03, baud])
         self.bus.send( msg, timeout=1 )
 
 
+    def int2hex(self, n):
+        h = format(n, 'x')
+        
+        if len(h) % 2 != 0:
+            leading = len(h)+1
+            h = h.zfill(leading)
+
+        return list(bytearray.fromhex(h)[::-1])
     
 def main(args=None):
     rclpy.init(args=args)
 
     encoder_publisher = EncoderPublisher()
+
+    encoder_publisher.query_mode(1)
 
     rclpy.spin(encoder_publisher)
 
