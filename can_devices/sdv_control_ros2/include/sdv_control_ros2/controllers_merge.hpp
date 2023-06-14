08c77b0f0e28759e67c8469ccf151ec789b5b874
Diego R <a01366488@itesm.mx>
2023-06-13 20:34:49 -0700

    Added PID and Stanley controller ROS2 nodes

---
+++ can_devices/sdv_control_ros2/include/sdv_control_ros2/controllers_merge.hpp
@@ -0,0 +1,40 @@
+#ifndef _CAR_FUSED_CONTROLLERS_
+#define _CAR_FUSED_CONTROLLERS_
+#include <memory>
+#include <chrono>
+#include "rclcpp/rclcpp.hpp"
+#include "sdv_msg/msg/eta_pose.hpp"
+#include "sdv_msg/msg/system_dynamics.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "common.hpp"
+#include <sdv_control_ros2/stanley_controller.hpp>
+#include <sdv_control_ros2/pid.hpp>
+
+namespace car_controller
+{
+    class CarController : public stanleycontroller::StanleyController, public pid::PID
+    {
+        public:
+            CarController(rclcpp::Node::SharedPtr node, float delta_max, float k, float k_soft, float sample_time, float k_p, float k_i, float k_d, float u_max, const DOFControllerType_E& type);
+            virtual ~CarController();
+            void timer_callback();
+
+        private:
+            rclcpp::Node::SharedPtr node_;
+            float delta_max;
+            float k;
+            float k_soft;
+            float sample_time;
+            float k_p;
+            float k_i;
+            float k_d;
+            float u_max;
+            DOFControllerType_E type;
+
+            rclcpp::Subscription<sdv_msg::msg::EtaPose>::SharedPtr stanley_heading;
+            rclcpp::Subscription<sdv_msg::msg::SystemDynamics>::SharedPtr car_dynamics;
+            rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr car_vel1;
+            rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr car_vel2;
+    };
+}
+#endif
