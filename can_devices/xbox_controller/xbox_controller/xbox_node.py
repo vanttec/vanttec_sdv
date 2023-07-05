9d4e86ec5547651be465c3a8d944541b6272f07d
vSebas <sebas.martp@gmail.com>
2023-07-04 19:52:16 -0600

    integrated throttle module into xbox node. testing tbd

--- can_devices/xbox_controller/xbox_controller/xbox_node.py
+++ can_devices/xbox_controller/xbox_controller/xbox_node.py
@@ -22,32 +22,34 @@ class XboxNode(Node):
         self.controller_stop = True
 
         self.joy_stick = xbox_driver.Joystick(50)
+        
+        timer_period = 0.1 #1 second
+        self.timer=self.create_timer(timer_period,self.xbox_timer)
+
+        self.bus = can.interface.Bus(bustype='socketcan', channel='can0', bitrate=125000)
 
         # *------------------* STEERING *------------------*
-        self.MAX_ANGLE = 57
-        self.STEP_ANGLE = 0.9
-        self.MAX_STEPS = 63
-        self.STEPS_REV = 400
+        # self.MAX_ANGLE = 57
+        # self.STEP_ANGLE = 0.9
+        # self.MAX_STEPS = 63
+        # self.STEPS_REV = 400
+        # self.req_steps = 0
+        # self.current_step = 0
+        # self.stepper_to_wheel_ratio = 1.5
 
-        self.req_steps = 0
-        self.current_step = 0
-        self.wheel_angle = 0
-
-        self.stepper_to_wheel_ratio = 1.5
+        self.steering_wheel_angle = 0
         # max steering = (wheel turns to max steer = 1.7) * (stepper to wheel ratio = 1.5) * 360 degrees
         self.max_steering = 918 # degrees
         
+        self.steering_module_id = 0x408
+        self.dir_id = 0x10
+
         # *------------------* THROTTLE *------------------*
-        self.safe_velocity=127
-        self.safe_pot = 127
-        #Send WiperPot position
-        self.pot_id = 384 #hex.180
-        #Send Max velocity
-        self.max_id = 385 #hex.181
-        self.receive_id = 386 #hex.182
+        self.safe_velocity=200 # % of safe_pot  
+        self.safe_pot = 170
         self.old_maxvel = self.safe_velocity
-        self.temp_pot=0
-        self.new_maxvel = self.safe_velocity #Can not surpass 80km/h  
+        self.temp_pot= 1
+        self.new_maxvel = self.safe_velocity
         self.old_increase_maxvel = 0 
         self.increase_maxvel = 0 
         self.old_decrease_maxvel = 0 
@@ -55,16 +57,19 @@ class XboxNode(Node):
         self.new_pot = 0
         self.old_pot = 0 
         self.limit_pot = self.safe_velocity
+        self.timer=self.create_timer(timer_period,self.throttle_timer)
+
+        self.throttle_module_id = 0x406
+        #Send WiperPot position
+        self.pot_id = 0x5
+        #Send Max velocity
+        self.max_id = 0x8
 
         # *------------------* ROS MESSAGES *------------------*
         self.xbox_info = XboxMsg()
         self.panel_info = PanelMsg()
         self.thottle_info = ThrottleMsg()
         self.vehicle_control = VehicleControl()
-        
-        # *------------------* TIMER *------------------*
-        timer_period = 0.1
-        self.timer = self.create_timer(timer_period, self.timer_callback)
 
         # *------------------* SUBSCRIBERS *------------------*
         self.encoder_sub = self.create_subscription(
@@ -77,12 +82,11 @@ class XboxNode(Node):
         # *------------------* PUBLISHERS *------------------*
         #self.xbox_status_pub = self.create_publisher(XboxMsg, 'xbox_controller/status', 10)
         self.panel_xbox_pub = self.create_publisher(PanelMsg, '/sdv/xbox_controller/xbox_panel', 10) 
-        self.throttle_xbox_pub = self.create_publisher(ThrottleMsg,'/sdv/xbox_controller/xbox_throttle',10)
         self.drive_mode_pub = self.create_publisher(String, '/sdv/xbox_controller/drive_mode', 10) 
         self.motor_mode_pub = self.create_publisher(String, '/sdv/xbox_controller/motor_mode', 10) 
         self.vehicle_control_pub = self.create_publisher(VehicleControl, '/sdv/manual_ctrl_cmd', 10)
         self.steering_pub = self.create_publisher(Vector3, '/steering_brake', 10)
-        self.pub_throttle_status = self.create_publisher(ThrottleMsg, 'throttle/status', 10)
+        # self.pub_throttle_status = self.create_publisher(ThrottleMsg, 'throttle/status', 10)
 
         self.drive_mode_dict = {
             "manual": can.Message(arbitration_id=self.admin_id,is_extended_id=False, data=[0x2,0x0]),
@@ -98,7 +102,7 @@ class XboxNode(Node):
     #     self.drive_mode = msg.data
 
     def encoder_callback(self,msg):
-        self.wheel_angle = msg.abs_angle
+        self.steering_wheel_angle = msg.abs_angle
 
     def panel_controller(self):
         self.panel_info.wiper.data = bool(self.xbox_info.a.data)
@@ -113,7 +117,23 @@ class XboxNode(Node):
         self.thottle_info.pot.data = self.xbox_info.right_trigger.data
         self.thottle_info.increase_maxvel.data = self.xbox_info.dpad_up.data
         self.thottle_info.decrease_maxvel.data =  self.xbox_info.dpad_down.data
-        self.throttle_xbox_pub.publish(self.thottle_info)
+
+        #Detect down bottom to decrease velocity
+        #Change max velocity
+        if bool(self.thottle_info.increase_maxvel.data):
+            if self.new_maxvel<=self.safe_velocity-5:
+                self.new_maxvel+=5
+        if bool(self.thottle_info.decrease_maxvel.data):
+            if self.new_maxvel>=5:
+                self.new_maxvel-=5
+        if self.old_maxvel != self.new_maxvel:
+            self.limit_pot = interp(self.new_maxvel, [0,100], [0,self.safe_pot])
+            self.old_maxvel=self.new_maxvel
+            self.get_logger().info('New max velocity: '+ str(self.new_maxvel)+" %")
+            self.get_logger().info('Pot Position: '+ str(self.limit_pot))
+            self.bus.send(can.Message(arbitration_id=self.throttle_module_id,is_extended_id=False, data=[self.max_id,int(self.limit_pot)]), timeout=1)
+        #Change pot position
+        self.new_pot = self.thottle_info.pot.data
 
     def lateral_control(self):
         msg = Vector3()
@@ -123,7 +143,7 @@ class XboxNode(Node):
         # self.vehicle_control_pub.publish(self.vehicle_control)
         joystick = self.xbox_info.leftx.data
         # self.get_logger().info("Joystick pos: %d" %joystick)
-        # self.get_logger().info("Wheel angle: %f" %self.wheel_angle)
+        # self.get_logger().info("Wheel angle: %f" %self.steering_wheel_angle)
         
         if(joystick != 0):
             dire = joystick / abs(joystick)
@@ -131,18 +151,26 @@ class XboxNode(Node):
             dire = 0
 
         if dire > 0:
-            if(self.max_steering - self.wheel_angle > 0):
+            if(self.max_steering - self.steering_wheel_angle > 0):
                 msg.x = float(dire)
         else:
             if dire < 0:
-                if(-self.max_steering - self.wheel_angle < 0):
+                if(-self.max_steering - self.steering_wheel_angle < 0):
                     msg.x = float(dire)
             else:
                 msg.x = 0.0
         
         msg.z = brake_data
 
-        self.steering_pub.publish(msg)
+        if msg.x > 0:
+            dir = 1     # Normal -> CW:0, CCW:1, but inverted due to gears
+        elif msg.x < 0:
+            dir = 0
+        else:
+            dir = 2
+
+        # self.steering_pub.publish(msg)
+        self.bus.send(can.Message(arbitration_id=self.steering_module_id,is_extended_id=False, data=[self.dir_id,int(dir)]), timeout=1)
 
     def publish_drive_mode(self):
         #Toggle car mode and pedal with XBOX controller   
@@ -167,7 +195,7 @@ class XboxNode(Node):
                 # self.bus.send(can.Message(arbitration_id=self.throttle_module_id,is_extended_id=False, data=[self.motor_mode_id,0x0]), timeout=1)      
         self.prev_start_btn_state = start_btn
 
-    def timer_callback(self):
+    def xbox_timer(self):
         if self.joy_stick.connected():
             self.publish_drive_mode()
             if self.drive_mode == "Controller":
@@ -192,6 +220,21 @@ class XboxNode(Node):
             # else:
             # self.get_logger().warn("Drive mode: " + self.drive_mode)
             # self.get_logger().info('Data: "%f"' % self.xbox_info.leftx.data)
+
+    def throttle_timer(self):
+        #Modo 2 (0-100%) en 100 segundos
+        if int(self.new_pot)>0:
+            self.temp_pot=100 if self.temp_pot>=100 else self.temp_pot+5
+            temp_pos = interp(self.temp_pot, [0,100], [1,self.limit_pot]) 
+            self.get_logger().info('Vel position: '+ str(self.temp_pot))
+            self.get_logger().info('Pot position: '+ str(temp_pos))
+            self.bus.send(can.Message(arbitration_id=self.throttle_module_id,is_extended_id=False,  data=[self.pot_id,int(temp_pos)]), timeout=1)
+        else:
+            self.temp_pot=0 if self.temp_pot<=0 else self.temp_pot-15
+            temp_pos = interp(self.temp_pot, [0,100], [1,self.limit_pot]) 
+            self.get_logger().info('Vel position: '+ str(self.temp_pot))
+            self.get_logger().info('Pot position: '+ str(temp_pos))
+            self.bus.send(can.Message(arbitration_id=self.throttle_module_id,is_extended_id=False,  data=[self.pot_id,int(temp_pos)]), timeout=1)
 
 def main(args=None):
     rclpy.init(args=args)
