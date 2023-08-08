5e2ef2a93d2008b8a48316396f80e9198d500bbc
vSebas <sebas.martp@gmail.com>
2023-08-07 19:14:53 -0600

    changes

--- docker_ws/sdv_localization/sdv_vectornav/sdv_vectornav/save2csv.py
+++ docker_ws/sdv_localization/sdv_vectornav/sdv_vectornav/save2csv.py
@@ -38,38 +38,48 @@ class IMU2CSV(Node):
         # self.attitude_sub_ = self.create_subscription( String, 'vectornav/raw/attitude',
         #                                               self.save_attitude, 1)
 
+        timer_period = 0.1  # seconds
+        self.timer = self.create_timer(timer_period, self.pot_callback)
+        self.pot_val = 0
+
         self.is_msg_arrived_ = False
         self.pot_step_val = 0
 
-        test = 'step90'
+        rosbag_path = '/home/ws/src/tests/'
+        test = 'pruebas1ago/throttle/step95/data'
 
-        self.accel_file_path_ = '/home/ws/src/tests/' + test + '/accel_data.csv'
+        self.accel_file_path_ = rosbag_path + test + '/accel_data.csv'
         self.accel_file_ = open(self.accel_file_path_, 'w')
         self.accel_file_writer_ = csv.writer(self.accel_file_)
         self.accel_file_writer_.writerow(['Time', 'AccelBody(x)','AccelBody(y)','AccelBody(z)', 'Theta', 'Psi', 'Psi_dot'])
 
-        self.vel_file_path_ = '/home/ws/src/tests/' + test + '/vel_data.csv'
+        self.vel_file_path_ = rosbag_path + test + '/vel_data.csv'
         self.vel_file_ = open(self.vel_file_path_, 'w')
         self.vel_file_writer_ = csv.writer(self.vel_file_)
         self.vel_file_writer_.writerow(['Time', 'VelBody(x)','VelBody(y)','VelBody(z)'])
 
-        self.encoder_file_path_ = '/home/ws/src/tests/' + test + '/encoder_data.csv'
+        self.encoder_file_path_ = rosbag_path + test + '/encoder_data.csv'
         self.encoder_file_ = open(self.encoder_file_path_, 'w')
         self.encoder_file_writer_ = csv.writer(self.encoder_file_)
         self.encoder_file_writer_.writerow(['Time', 'WheelAngle', 'SteeringAngle'])
 
-        self.pot_file_path_ = '/home/ws/src/tests/' + test + '/pot_data.csv'
+        self.pot_file_path_ = rosbag_path + test + '/pot_data.csv'
         self.pot_file_ = open(self.pot_file_path_, 'w')
         self.pot_file_writer_ = csv.writer(self.pot_file_)
         self.pot_file_writer_.writerow(['Time', 'Pot_Step'])
+
+    def pot_callback(self):
+        if self.is_msg_arrived_:
+            elapsed_time = self.get_clock().now() - self.start_time_
+            self.pot_file_writer_.writerow([elapsed_time.nanoseconds / 1e9, self.pot_val])
 
     def pot_step(self, msg):
         if not self.is_msg_arrived_:
             self.is_msg_arrived_ = True
             self.start_time_ = self.get_clock().now()
-
-        elapsed_time = self.get_clock().now() - self.start_time_
-        self.accel_file_writer_.writerow([elapsed_time.nanoseconds / 1e9, msg.data])
+            
+        if self.pot_val != msg.data:
+            self.pot_val = msg.data
 
     def save_common(self, msg):
         # if not self.is_msg_arrived_:
@@ -99,16 +109,16 @@ class IMU2CSV(Node):
             MAX_WHEEL_ANGLE = 715.78
             MIN_WHEEL_ANGLE = -577
 
-        MAX_DELTA_R = 44
-        MIN_DELTA_R = -44 #TO CHECK
+            MAX_DELTA_R = 44
+            MIN_DELTA_R = -44 #TO CHECK
 
-        MAX_DELTA_L = 38 #TO CHECK
-        MIN_DELTA_L = -38
-        
-        delta_r = msg.abs_angle * MAX_DELTA_R/MAX_WHEEL_ANGLE if msg.abs_angle >= 0 else msg.abs_angle * MIN_DELTA_R/MIN_WHEEL_ANGLE
-        delta_l = msg.abs_angle * MAX_DELTA_L/MAX_WHEEL_ANGLE if msg.abs_angle >= 0 else msg.abs_angle * MIN_DELTA_L/MIN_WHEEL_ANGLE
-        delta = (delta_l + delta_r) / 2
-        self.encoder_file_writer_.writerow([elapsed_time.nanoseconds / 1e9, msg.abs_angle, delta])
+            MAX_DELTA_L = 38 #TO CHECK
+            MIN_DELTA_L = -38
+            
+            delta_r = msg.abs_angle * MAX_DELTA_R/MAX_WHEEL_ANGLE if msg.abs_angle >= 0 else msg.abs_angle * MIN_DELTA_R/MIN_WHEEL_ANGLE
+            delta_l = msg.abs_angle * MAX_DELTA_L/MAX_WHEEL_ANGLE if msg.abs_angle >= 0 else msg.abs_angle * MIN_DELTA_L/MIN_WHEEL_ANGLE
+            delta = (delta_l + delta_r) / 2
+            self.encoder_file_writer_.writerow([elapsed_time.nanoseconds / 1e9, msg.abs_angle, delta])
 
     def close(self):
         self.accel_file_.close()
