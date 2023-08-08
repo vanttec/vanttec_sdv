1b152e1c04d80b767c866ae81e8c67babe23b150
EdisonAltamirano <A00825234@itesm.mx>
2023-08-08 01:44:24 +0000

    fix encoder

--- README.md
+++ README.md
@@ -31,8 +31,6 @@ sudo make sdv.up
 sudo make sdv.shell
 cd /home/ws/
 git clone --recurse-submodules https://github.com/vanttec/sdv_ros.git
-cd src/sdv_ros/can_devices/sdv_can/libs/vanttec_CANLib/
-git checkout feature/sdv_can
 cd /home/ws/
 colcon build
 
--- can_devices/encoder/encoder/encoder_briter.py
+++ can_devices/encoder/encoder/encoder_briter.py
@@ -11,7 +11,7 @@ class EncoderPublisher(Node):
 
     def __init__(self):
         super().__init__('encoder_rm')
-        self.publisher = self.create_publisher(Encoder, 'encoder/freno', 10)
+        self.publisher = self.create_publisher(Encoder, 'encoder_freno', 10)
         timer_period = 0.01 #Seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.bus = can.interface.Bus(bustype='socketcan', channel='can0', bitrate=125000)
@@ -24,7 +24,7 @@ class EncoderPublisher(Node):
         receivedMsg = self.bus.recv(1)
 
         if receivedMsg is not None:
-            if receivedMsg.is_rx:
+            if receivedMsg.arbitration_id == 0x1:
                 decoded_msg = receivedMsg.data.hex()[6:]
                 hex_pos = (decoded_msg[6:7]+decoded_msg[4:6]+decoded_msg[2:4]+decoded_msg[0:2])
                 absolute_pos = int(hex_pos, 16)
