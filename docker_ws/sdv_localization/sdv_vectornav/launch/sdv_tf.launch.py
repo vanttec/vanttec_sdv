44050ed1393448b075830a17c956f541f9c07465
vSebas <sebas.martp@gmail.com>
2023-08-27 17:44:56 -0600

    modified launch files to run from a single one

--- docker_ws/sdv_localization/sdv_vectornav/launch/sdv_tf.launch.py
+++ docker_ws/sdv_localization/sdv_vectornav/launch/sdv_tf.launch.py
@@ -8,18 +8,27 @@ from launch.substitutions import LaunchConfiguration
 
 from launch_ros.actions import Node
 
+from launch.conditions import UnlessCondition
+
 def generate_launch_description():
 
     this_dir = get_package_share_directory('sdv_vectornav')
-    
+
+    is_simulation = DeclareLaunchArgument(
+      'is_simulation',
+      default_value = 'false',
+      description = 'Defines if the application will run in simulation or in real life'
+    )
+
     # Vectornav odometry and path
     start_odom_pub = Node(
-        package='sdv_vectornav', 
-        executable='vn_gps_pose',
-        output='screen',
-        parameters=[os.path.join(this_dir, 'config', 'vn_gps_node_params.yaml')])
+      package='sdv_vectornav', 
+      executable='vn_gps_pose',
+      output='screen',
+      parameters=[os.path.join(this_dir, 'config', 'vn_gps_node_params.yaml')],
+      condition=UnlessCondition(LaunchConfiguration('is_simulation'))
+    )
     
-
     #ODOM is in NED frame since vn measurements are in that configuration, so the rotation from NED to ENU for MAP is necessary
     start_transform_odom_base_link = Node(
             package='tf2_ros',
@@ -30,6 +39,7 @@ def generate_launch_description():
     # Create the launch description and populate
     ld = LaunchDescription()
 
+    ld.add_action(is_simulation)
     ld.add_action(start_odom_pub)
     ld.add_action(start_transform_odom_base_link)
     return ld
