e657c7f63016200c192ade2cfc75b05166cf3624
vSebas <sebas.martp@gmail.com>
2023-09-01 00:57:55 -0600

    changes

--- docker_ws/sdv_localization/sdv_localization/src/vn_processing.cc
+++ docker_ws/sdv_localization/sdv_localization/src/vn_processing.cc
@@ -220,16 +220,16 @@ private:
         /* TRANSFORM BROADCASTER */
 
         //Transform odom to base_link publish in a ENU frame
-        geometry_msgs::msg::TransformStamped tf;
+        // geometry_msgs::msg::TransformStamped tf;
 
-        tf.header.frame_id = "odom";
-        tf.header.set__stamp(msg_in->header.stamp);
-        tf.child_frame_id = "base_link";
-        tf.transform.translation.x = enu_pose_msg.pose.pose.position.x;  //
-        tf.transform.translation.y = enu_pose_msg.pose.pose.position.y;  //
-        tf.transform.translation.z = enu_pose_msg.pose.pose.position.z;  //
-        tf.transform.set__rotation(enu_pose_msg.pose.pose.orientation);  //
-        odom_tf_broadcaster_->sendTransform(tf);
+        // tf.header.frame_id = "odom";
+        // tf.header.set__stamp(msg_in->header.stamp);
+        // tf.child_frame_id = "base_link";
+        // tf.transform.translation.x = enu_pose_msg.pose.pose.position.x;  //
+        // tf.transform.translation.y = enu_pose_msg.pose.pose.position.y;  //
+        // tf.transform.translation.z = enu_pose_msg.pose.pose.position.z;  //
+        // tf.transform.set__rotation(enu_pose_msg.pose.pose.orientation);  //
+        // odom_tf_broadcaster_->sendTransform(tf);
     }
     else
     {
