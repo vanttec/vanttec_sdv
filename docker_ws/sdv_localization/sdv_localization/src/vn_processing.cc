f7084706070ae311a5a3c808a8d6c5f8ae1c0d08
DRogerSC <musicfan211@hotmail.com>
2023-09-03 03:11:48 -0600

    Initial commit to branch

--- docker_ws/sdv_localization/sdv_localization/src/vn_processing.cc
+++ docker_ws/sdv_localization/sdv_localization/src/vn_processing.cc
@@ -173,7 +173,7 @@ private:
         mat.getRPY(roll, pitch, yaw);
 
         tf2::Quaternion quat;
-        quat.setRPY(0, 0, yaw);
+        quat.setRPY(0, 0, -yaw);
         // quat.setYPR(yaw, pitch, roll);
         geometry_msgs::msg::Quaternion q;
         q.x = quat.x(); 
@@ -181,21 +181,21 @@ private:
         q.z = quat.z();
         q.w = quat.w();
 
-        // tf2::Quaternion quaternion1( q.x,
-        //                             q.y,
-        //                             q.z, 
-        //                             q.w);
-        // tf2::Matrix3x3 mat2(quaternion1);
+        tf2::Quaternion quaternion1( q.x,
+                                    q.y,
+                                    q.z, 
+                                    q.w);
+        tf2::Matrix3x3 mat2(quaternion1);
 
-        // mat2.getRPY(roll, pitch, yaw);
+        mat2.getRPY(roll, pitch, yaw);
 
-        // std::cout << "Vectornav" << std::endl;
-        // std::cout << "yaw = " << yaw * 180 / M_PI << " pitch = " << pitch * 180 / M_PI << " roll = " << roll * 180 / M_PI<< std::endl;
+        std::cout << "Vectornav" << std::endl;
+        std::cout << "yaw = " << yaw * 180 / M_PI << " pitch = " << pitch * 180 / M_PI << " roll = " << roll * 180 / M_PI<< std::endl;
 
-        // tf2::Quaternion quaternionResult;
-        // tf2::convert(msg_in->quaternion, quaternionResult);
-        // quaternionResult.setZ(-quaternionResult.getZ());
-        // quaternionResult.normalize();
+        tf2::Quaternion quaternionResult;
+        tf2::convert(msg_in->quaternion, quaternionResult);
+        quaternionResult.setZ(-quaternionResult.getZ());
+        quaternionResult.normalize();
 
         // enu_pose_msg.pose.pose.orientation = tf2::toMsg(quaternionResult);
         enu_pose_msg.pose.pose.orientation = q;
@@ -248,16 +248,16 @@ private:
         /* TRANSFORM BROADCASTER */
 
         //Transform odom to base_link publish in a ENU frame
-        // geometry_msgs::msg::TransformStamped tf;
+        geometry_msgs::msg::TransformStamped tf;
 
-        // tf.header.frame_id = "odom";
-        // tf.header.set__stamp(msg_in->header.stamp);
-        // tf.child_frame_id = "base_link";
-        // tf.transform.translation.x = enu_pose_msg.pose.pose.position.x;  //
-        // tf.transform.translation.y = enu_pose_msg.pose.pose.position.y;  //
-        // tf.transform.translation.z = enu_pose_msg.pose.pose.position.z;  //
-        // tf.transform.set__rotation(enu_pose_msg.pose.pose.orientation);  //
-        // odom_tf_broadcaster_->sendTransform(tf);
+        tf.header.frame_id = "odom";
+        tf.header.set__stamp(msg_in->header.stamp);
+        tf.child_frame_id = "base_link";
+        tf.transform.translation.x = enu_pose_msg.pose.pose.position.x;  //
+        tf.transform.translation.y = enu_pose_msg.pose.pose.position.y;  //
+        tf.transform.translation.z = enu_pose_msg.pose.pose.position.z;  //
+        tf.transform.set__rotation(enu_pose_msg.pose.pose.orientation);  //
+        odom_tf_broadcaster_->sendTransform(tf);
     }
     else
     {
