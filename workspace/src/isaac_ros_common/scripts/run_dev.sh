42ca6ebbdafd03c890f9f138c18229271ffb8e52
romi2002 <empoleom@gmail.com>
2024-02-26 23:27:31 -0600

    Changed run_dev to use normal workspace

--- workspace/src/isaac_ros_common/scripts/run_dev.sh
+++ workspace/src/isaac_ros_common/scripts/run_dev.sh
@@ -191,19 +191,19 @@ if [[ -f "$DOCKER_ARGS_FILE" ]]; then
 fi
 
 # Run container from image
-print_info "Running $CONTAINER_NAME"
+print_info "Running $CONTAINER_NAME in $ISAAC_ROS_DEV_DIR"
 docker run -it --rm \
     --privileged \
     --network host \
     ${DOCKER_ARGS[@]} \
-    -v $ISAAC_ROS_DEV_DIR:/workspaces/isaac_ros-dev \
+    -v $ISAAC_ROS_DEV_DIR:/workspace \
     -v /dev/*:/dev/* \
     -v /etc/localtime:/etc/localtime:ro \
     --name "$CONTAINER_NAME" \
     --runtime nvidia \
     --user="admin" \
     --entrypoint /usr/local/bin/scripts/workspace-entrypoint.sh \
-    --workdir /workspaces/isaac_ros-dev \
+    --workdir /workspace \
     $@ \
     $BASE_NAME \
     /bin/bash
