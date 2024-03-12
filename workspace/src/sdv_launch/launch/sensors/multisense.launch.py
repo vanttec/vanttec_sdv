c1e62f2a893dacd983d1117365e69ed3fe7a32c4
FCANOF <a01752953@tec.mx>
2024-03-12 17:26:38 -0600

    Multisense SDV Launch File

---
+++ workspace/src/sdv_launch/launch/sensors/multisense.launch.py
@@ -0,0 +1,44 @@
+# SDV - MULTISENSE Launch file
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration, TextSubstitution, Command, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackagePrefix
+
+
+def generate_launch_description():
+
+    sensor = DeclareLaunchArgument(name='sensor',
+                                   default_value='S21',
+                                   description='Type of multisense: S21, SL, S7, S7S, S27, S30, KS21')
+
+    namespace = DeclareLaunchArgument(name='namespace',
+                                      default_value='multisense',
+                                      description='Namespace for this MultiSense instance')
+
+    mtu = DeclareLaunchArgument(name='mtu',
+                                default_value='7200',
+                                description='Sensor MTU')
+
+    ip_address = DeclareLaunchArgument(name='ip_address',
+                                       default_value='10.66.171.21',
+                                       description='Sensor IP address')
+
+
+    multisense_ros = Node(package='multisense_ros',
+                         namespace=[LaunchConfiguration('namespace')],
+                         executable='ros_driver',
+                         parameters=[{'sensor_ip': LaunchConfiguration('ip_address'),
+                                      'sensor_mtu': LaunchConfiguration('mtu'),
+                                      'tf_prefix': LaunchConfiguration('namespace')}])
+
+
+    return LaunchDescription([sensor,
+                              namespace,
+                              mtu,
+                              ip_address,
+                              multisense_ros])
