9a85bff3c52be6dfd86f9014453a7bd7da94bc5a
FCANOF <a01752953@tec.mx>
2024-03-21 14:27:22 -0600

    Update urd configuration

--- workspace/src/sdv_launch/launch/sensors/velodyne.launch.py
+++ workspace/src/sdv_launch/launch/sensors/velodyne.launch.py
@@ -25,7 +25,7 @@ def generate_launch_description():
        package='tf2_ros',
        executable='static_transform_publisher',
        name="tf_base_link_to_velodyne",
-       arguments = ['0.45', '0', '2.25', '-0.05', '0.0', '0.0', 'base_link', 'velodyne'],  #-0.05 y
+       arguments = ['0', '0', '1.8', '-0.174533', '0.0', '0.0', 'base_link', 'velodyne'],  #-0.05 y
        condition=UnlessCondition(LaunchConfiguration('simulation'))
     )
 
