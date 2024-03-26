4442d3dfd0dae6bbee1bfbd1afee73c5b86ed4e7
Abiel Fernandez <empoleom@gmail.com>
2024-03-26 01:45:30 -0600

    Updated run_dev.sh such that docker can start offline

--- workspace/src/isaac_ros_common/scripts/run_dev.sh
+++ workspace/src/isaac_ros_common/scripts/run_dev.sh
@@ -136,12 +136,14 @@ if [[ ! -z "${IMAGE_KEY}" ]]; then
     fi
 fi
 
-print_info "Building $BASE_IMAGE_KEY base as image: $BASE_NAME using key $BASE_IMAGE_KEY"
-$ROOT/build_base_image.sh $BASE_IMAGE_KEY $BASE_NAME '' '' ''
+if ping -c 1 1.1.1.1 >/dev/null 2>&1; then
+    print_info "Building $BASE_IMAGE_KEY base as image: $BASE_NAME using key $BASE_IMAGE_KEY"
+    $ROOT/build_base_image.sh $BASE_IMAGE_KEY $BASE_NAME '' '' ''
 
-if [ $? -ne 0 ]; then
-    print_error "Failed to build base image: $BASE_NAME, aborting."
-    exit 1
+    if [ $? -ne 0 ]; then
+        print_error "Failed to build base image: $BASE_NAME, aborting."
+        exit 1
+    fi
 fi
 
 # Map host's display socket to docker
