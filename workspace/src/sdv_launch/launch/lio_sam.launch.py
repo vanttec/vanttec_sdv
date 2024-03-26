3d44d2c0de6da86a14578b3066e941a8e66d0679
Abiel <empoleom@gmail.com>
2024-03-26 01:23:15 -0600

    Added base LIO_SAM configs

---
+++ workspace/src/sdv_launch/launch/lio_sam.launch.py
@@ -0,0 +1,71 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, Command
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    launch_dir = get_package_share_directory('sdv_launch')
+    description_dir = get_package_share_directory('sdv_robot_description')
+    parameter_file = LaunchConfiguration('params_file')
+    xacro_path = os.path.join(description_dir, 'urdf', 'robot.urdf')
+
+    params_declare = DeclareLaunchArgument(
+        'params_file',
+        default_value=os.path.join(
+            launch_dir, 'config', 'lio_sam', 'params.yaml'),
+        description='FPath to the ROS2 parameters file to use.')
+
+    print("parameter file: {}".format(parameter_file))
+    print("urdf_file_name : {}".format(xacro_path))
+
+    return LaunchDescription([
+        params_declare,
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            arguments='0.0 0.0 0.0 0.0 0.0 0.0 map odom'.split(' '),
+            parameters=[parameter_file],
+            output='screen'
+            ),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{
+                'robot_description': Command(['xacro', ' ', xacro_path])
+            }]
+        ),
+        Node(
+            package='lio_sam',
+            executable='lio_sam_imuPreintegration',
+            name='lio_sam_imuPreintegration',
+            parameters=[parameter_file],
+            output='screen'
+        ),
+        Node(
+            package='lio_sam',
+            executable='lio_sam_imageProjection',
+            name='lio_sam_imageProjection',
+            parameters=[parameter_file],
+            output='screen'
+        ),
+        Node(
+            package='lio_sam',
+            executable='lio_sam_featureExtraction',
+            name='lio_sam_featureExtraction',
+            parameters=[parameter_file],
+            output='screen'
+        ),
+        Node(
+            package='lio_sam',
+            executable='lio_sam_mapOptimization',
+            name='lio_sam_mapOptimization',
+            parameters=[parameter_file],
+            output='screen'
+        )
+    ])
