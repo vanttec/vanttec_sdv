b9c3d1110fe88de1f00997ee538cb6bc9eff5fab
Rob <106797059+rpribau@users.noreply.github.com>
2024-05-17 10:33:11 -0600

    C++ people_detection created.

---
+++ workspace/src/sdv_vision/yolov8_object_detection/people_distance_detection/main.h
@@ -0,0 +1,11 @@
+#ifndef YOLOV8_MAIN_H
+#define YOLOV8_MAIN_H
+
+#include "yolo/YoloPose.h"
+#include "tools/ImageTools.h"
+
+
+void yoloPose(cv::Mat &image, YoloPose &yolo);
+
+
+#endif //YOLOV8_MAIN_H
